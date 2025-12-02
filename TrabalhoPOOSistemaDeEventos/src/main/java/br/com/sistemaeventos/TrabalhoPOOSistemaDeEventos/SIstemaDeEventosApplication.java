9ab2ce330db2eb5957d462062adf6ec2741b00ef
Tetocas <72585097+Tetocas@users.noreply.github.com>
2025-12-02 18:22:44 -0300

    Add files via upload
    Trabalho de POO

---
+++ TrabalhoPOOSistemaDeEventos/src/main/java/br/com/sistemaeventos/TrabalhoPOOSistemaDeEventos/SIstemaDeEventosApplication.java
@@ -0,0 +1,13 @@
+package br.com.sistemaeventos.TrabalhoPOOSistemaDeEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SIstemaDeEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SIstemaDeEventosApplication.class, args);
+	}
+
+}
